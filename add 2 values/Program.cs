62fb886dab1177c18997115a47c4b861797020f5
Reballi <srikanth.raj18@gmail.com>
2022-04-12 21:50:47 +0530

    first commit

---
+++ add 2 values/Program.cs
@@ -0,0 +1,14 @@
+﻿class program
+{
+    static void Main(string[]args)
+    {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        Console.Write("{0}+{1}={2}",a,b,c);
+
+
+
+    }
+
+}
